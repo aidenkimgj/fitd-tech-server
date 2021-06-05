c4db9b22d40bd0f0e8aab0324dc5bb1ff70abd13
Aiden Kim <aidengookjinkim@gmail.com>
2021-06-04 20:20:21 -0600

    index.js init

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 import express from 'express';
 import cors from 'core';
-import config from './config/index'
+import config from './config/index';
 import mongoose from 'mongoose';
-import hpp from 'hpp'
-import helmet from 'helmet'
-import 
+import hpp from 'hpp';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import path from 'path';
+
+// Routes
+import userRoutes from './routes/api/user.js';
+import contentRoutes from './routes/api/content.js';
+
+const app = express();
+const { MONGO_URI } = config;
+app.use(hpp());
+app.use(helmet({ contentSecurityPolicy: false }));
+app.use(cors({ origin: true, credentials: true, preFlightContinue: true }));
+app.use(morgan('dev'));
+app.use(express.json());
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('MongoDB connected!'))
+  .catch(err => {
+    throw err;
+  });
+
+// Use routes
+app.use('/api/user', userRoutes);
+
+export default app;
