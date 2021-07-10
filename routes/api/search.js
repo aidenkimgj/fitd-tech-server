01b0b1e4a58b12e4d2a5129825a24a0298d0d02a
Aiden Kim <aidengookjinkim@gmail.com>
2021-07-10 00:36:37 -0600

    Add coach search API

--- routes/api/search.js
+++ routes/api/search.js
@@ -2,6 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 import Content from '../../models/content';
+import NewCoach from '../../models/newCoach';
 
 //========================================
 //         Search Api
@@ -9,15 +10,37 @@ import Content from '../../models/content';
 //========================================
 
 /*
- * @route    GET api/search/:searchTerm
+ * @route    GET api/search/content:searchTerm
  * @desc     search name retrive
  * @access   Public
  *
  */
-router.get('/:searchTerm', async (req, res, next) => {
+router.get('/content/:searchTerm', async (req, res, next) => {
   try {
     const result = await Content.find({
       title: {
+        $regex: req.params.searchTerm,
+        $options: 'i',
+      },
+    });
+    console.log(result, 'Search result');
+    res.json(result);
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
+
+/*
+ * @route    GET api/search/content:searchTerm
+ * @desc     search name retrive
+ * @access   Public
+ *
+ */
+router.get('/coach/:searchTerm', async (req, res, next) => {
+  try {
+    const result = await NewCoach.find({
+      expertiseArea: {
         $regex: req.params.searchTerm,
         $options: 'i',
       },
