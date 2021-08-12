94cc882504a298167fdfabd9b9a86ad4f127d853
Aiden Kim <aidengookjinkim@gmail.com>
2021-08-12 10:39:31 -0600

    Finalize

--- server.js
+++
@@ -1,8 +0,0 @@
-import app from './app.js';
-import config from './config/index.js';
-
-const { PORT } = config;
-
-app.listen(PORT, () => {
-  console.log(`Server On: http://localhost:${PORT}/`);
-});
